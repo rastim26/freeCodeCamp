ed84a04ce06f90513c5e3d745e83ec082ab351f6
rastim26 <rast875@gmail.com>
2021-10-27 01:27:34 +0300

    elseif

--- main.js
+++ main.js
@@ -27,3 +27,15 @@ if (a + b < 4) {
 }
 
 result = a + b < 4 ? (result = "Мало") : (result = "Много");
+
+let message;
+
+if (login == "Сотрудник") {
+  message = "Привет";
+} else if (login == "Директор") {
+  message = "Здравствуйте";
+} else if (login == "") {
+  message = "Нет логина";
+} else {
+  message = "";
+}
