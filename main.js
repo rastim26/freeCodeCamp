e668e96c4b09c8c78d17859ded97b1d0e37114dc
rastim26 <rast875@gmail.com>
2021-10-27 01:27:15 +0300

    ifelse

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ console.log("Hello World");
 
 let JsName = prompt("Какое 'официальное' название JavaScript?", "");
 
-if ((JsName = "ECMAScript")) {
+if (JsName == "ECMAScript") {
   alert("Правильно!");
 } else {
   alert("Не знаете? “ECMAScript”!");
 }
+
+let myNum = prompt("Enter your number:", "");
+
+if (myNum > 0) {
+  alert("1");
+} else if (myNum < 0) {
+  alert("-1");
+} else {
+  alert("0");
+}
+
+let result;
+
+if (a + b < 4) {
+  result = "Мало";
+} else {
+  result = "Много";
+}
+
+result = a + b < 4 ? (result = "Мало") : (result = "Много");
