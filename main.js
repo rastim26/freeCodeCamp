9fa8527d60193ee2548636d9d2912915587ab2f4
rastim26 <rast875@gmail.com>
2021-10-27 01:11:43 +0300

    JsName

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+let JsName = prompt("Какое 'официальное' название JavaScript?", "");
+
+if ((JsName = "ECMAScript")) {
+  alert("Правильно!");
+} else {
+  alert("Не знаете? “ECMAScript”!");
+}
