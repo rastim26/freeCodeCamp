842269ac5e9c31aca34899de6bdfda41e56d4d47
rastim26 <rast875@gmail.com>
2021-11-03 00:21:15 +0300

    Switch to If else

--- tasks.js
+++ tasks.js
@@ -114,3 +114,32 @@ for (let i = 2; i <= 10; i++) {
     continue;
   }
 } // ????????????????????
+
+// Конструкция "switch"
+
+switch (browser) {
+  case "Edge":
+    alert("You've got the Edge!");
+    break;
+
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    alert("Okay we support these browsers too");
+    break;
+
+  default:
+    alert("We hope that this page looks ok!");
+}
+
+if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
