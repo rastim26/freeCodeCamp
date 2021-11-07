d4a5cd196d00afb7280f858dfb0d0204cea5841d
rastim26 <rast875@gmail.com>
2021-11-07 13:12:26 +0300

    Objects 5

--- main.js
+++ main.js
@@ -130,3 +130,43 @@ for (let person in salaries) {
   sum += salaries[person];
 }
 alert(sum);
+
+// Умножаем все числовые свойства на 2
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+multiplyNumeric(menu);
+
+// после вызова функции
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu",
+};
+// Обратите внимание, что multiplyNumeric не нужно ничего возвращать. Следует напрямую изменять объект.
+
+// P.S. Используйте typeof для проверки, что значение свойства числовое.
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] *= 2;
+    }
+  }
+}
+
+multiplyNumeric(menu);
+
+console.log(menu);
