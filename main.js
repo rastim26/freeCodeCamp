1a7c01a3de46c1c4f481179eef4cf29b886c0575
rastim26 <rast875@gmail.com>
2021-11-07 12:08:46 +0300

    Objects 2

--- main.js
+++ main.js
@@ -104,3 +104,15 @@ user.name = "John";
 user.surname = "Smith";
 user.name = "Pete";
 delete user.name;
+
+// Проверка на пустоту
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+// Должно работать так:
+
+function isEmpty(obj) {
+  for (let prop in obj) {
+    return false;
+  }
+  return true;
+}
