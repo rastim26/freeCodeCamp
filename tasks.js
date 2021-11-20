fe7ca5201ceec6abccf15f08f5bb86400f42e671
rastim26 <rast875@gmail.com>
2021-11-20 21:30:15 +0300

    Arrays Task 2

--- tasks.js
+++ tasks.js
@@ -548,3 +548,18 @@ function readNumber() {
 }
 
 readNumber();
+
+// Операции с массивами
+
+let styles = ["Джаз", "Блюз"];
+
+styles.push("Рок-н-ролл");
+
+styles[styles.length % 2 === 0 ? styles.length / 2 : (styles.length - 1) / 2] =
+  "Классика";
+
+console.log(styles.shift());
+
+styles.unshift("Рэп", "Регги");
+
+console.log(styles);
